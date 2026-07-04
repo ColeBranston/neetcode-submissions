6408e93b738295fd78d7f0ee6cfbc953c269bfe2
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-07-04 04:39:10 -0400

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,47 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        res = []
+
+        # NOTE: WIll take some visualization when coming back to it
+
+        i = 0
+
+        '''
+        Three check method where we cover first the intervals completely excluded, 
+        second the overlapping intervals, and then finally everything else
+        '''
+        
+        '''
+        checks for all intervals before the new interval checking the 
+        end time of curr is < than the start of the new 
+        '''
+        while i < n and newInterval[0] > intervals[i][1]:
+            res.append(intervals[i])
+            i+=1
+
+        '''
+        based on the previous check we know that the start time of the new interval is less than or equal
+        to the end time of the current interval, therefore the start time is inside the interval
+
+        + we also check if the end time of the new interval is greater than or equal to the start time
+        of the current interval
+
+        this combination of logic basically ensures that we are only qualifying intervals that are either
+        overlapping, new is inside of the current interval or the current interval is inside of new
+        '''
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i+=1
+
+        res.append(newInterval)
+
+        '''
+        Append the rest
+        '''
+        while i < n:
+            res.append(intervals[i])
+            i+=1
+
+        return res
