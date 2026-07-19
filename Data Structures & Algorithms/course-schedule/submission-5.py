8f5ffabbde518fe4a9483d61157a364e19464a11
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-07-19 00:14:27 -0400

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+
+        for a,b in prerequisites:
+            adj[b].append(a) # you must go to indice b before a
+
+        visit = set()
+
+        def dfs(node):
+            if node in visit:
+                return False
+
+            visit.add(node)
+
+            for nextt in adj[node]: # go to every course after this prereq
+                if not dfs(nextt):
+                    return False # if there is a cycle detection later down the chain, continue it up the chain
+
+            visit.remove(node)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
