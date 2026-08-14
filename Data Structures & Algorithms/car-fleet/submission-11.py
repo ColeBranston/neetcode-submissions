de2a4927fce501b2ce5e118858375b927cbe56a1
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-08-14 02:48:16 -0400

    Add: car-fleet - submission-11

---
+++ Data Structures & Algorithms/car-fleet/submission-11.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        prev = None
+
+        cars = sorted(zip(position, speed))
+
+        for car in cars:
+            time = (target - car[0]) / car[1]
+            stack.append(time)
+
+        first = stack.pop()
+        res = [first]
+        prev = first
+
+        while stack:
+            nextCar = stack.pop()
+
+            if nextCar > prev:
+                res.append(nextCar)
+                prev = nextCar
+
+        return len(res)
