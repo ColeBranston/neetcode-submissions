ac5c0200256fb14d7a2dd4fa77e099a410a44552
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-08-26 16:12:33 -0400

    Add: buy-and-sell-crypto - submission-18

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-18.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # maxx = 0
+        # for i in range(len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         maxx = max(maxx, prices[j]-prices[i])
+
+        # return maxx
+        # Time: O(n^2)
+        # Space: O(1)
+
+        l = 0
+        maxx = 0
+        for r in range(len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            print(prices[l], prices[r])
+            maxx = max(maxx, prices[r] - prices[l])
+
+        return maxx
+
+
+
