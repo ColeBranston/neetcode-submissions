75b5fa4c4167ee15e5d318980475ac9bf92c5d72
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-09-04 07:10:28 -0400

    Add: reverse-nodes-in-k-group - submission-15

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-15.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        def getKth(node, k):
+            while node and k > 0:
+                node = node.next
+                k-=1
+            return node
+
+        dummy = start = ListNode(0,head)
+
+        while True:
+            kth = getKth(start, k)
+            if not kth:
+                break
+                
+            stop = kth.next
+            prev = stop
+            curr = start.next
+
+            while curr != stop:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+
+            temp = start.next
+            start.next = kth # this actually completes the reversal by ensuring that before the start is moved to the next node (previously unreversed, now reversed), that the kth node is wired in. Ex. on every first group for 1,2,3,4 k=3, dummy.next is 1 but then everything gets reversed and it becomes 3,2,1,4, now to ensure that everything continues, start.next ie. dummy.next on the first time is set to kth or 3, then start is set to 1 for the next itteration and it continues.
+            start = temp
+
+
+        return dummy.next
