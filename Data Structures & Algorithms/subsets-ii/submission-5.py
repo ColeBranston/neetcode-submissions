bb29a90dff5d7db6c4e599d2f136b698ce099522
Cole Branston <118498961+ColeBranston@users.noreply.github.com>
2026-09-23 02:44:11 -0400

    Add: subsets-ii - submission-5

---
+++ Data Structures & Algorithms/subsets-ii/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(curr, index):
+            nonlocal res
+           
+            res.append(curr.copy())
+
+            for i in range(index, len(nums)):
+                if i > index and nums[i] == nums[i-1]: # same as before still counts
+                    continue
+
+                curr.append(nums[i])
+                backtrack(curr, i+1)
+                curr.pop()
+
+        backtrack([], 0)
+        return res
